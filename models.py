c366fe64a1577ccd88f8eb04df2065b2ae7f7c6f
PAPpython <2024tgeigrupo8@gmail.com>
2026-01-17 22:39:56 +0000

    Update models.py

--- models.py
+++ models.py
@@ -7,8 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     email = db.Column(db.String(120), unique=True, nullable=False)
-    username = db.Column(db.String(80))
     password_hash = db.Column(db.String(200))
 
-    recovery_code = db.Column(db.String(64))
+    recovery_code = db.Column(db.String(10))
     recovery_expires = db.Column(db.DateTime)
