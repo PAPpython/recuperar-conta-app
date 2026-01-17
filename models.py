d01c2242c5376e8049cc1fcf6f4ce8d7cce15965
PAPpython <2024tgeigrupo8@gmail.com>
2026-01-17 22:31:56 +0000

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,14 @@
-from recuperadordecontas import db
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    username = db.Column(db.String(80), nullable=False)
-    password = db.Column(db.String(256), nullable=False)
 
-class RecoveryCode(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), nullable=False)
-    code = db.Column(db.String(6), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80))
+    password_hash = db.Column(db.String(200))
+
+    recovery_code = db.Column(db.String(64))
+    recovery_expires = db.Column(db.DateTime)
