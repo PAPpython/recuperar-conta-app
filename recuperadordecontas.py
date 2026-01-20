d540fd911e1fe1673a2f8ee1b89ade958997c2d6
PAPpython <2024tgeigrupo8@gmail.com>
2026-01-20 01:11:59 +0000

    Update recuperadordecontas.py

--- recuperadordecontas.py
+++ recuperadordecontas.py
@@ -181,5 +181,9 @@ def check_email():
     )
 
 # ================= START =================
+   if __name__ == "__main__":
     with app.app_context():
         db.create_all()
+
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
