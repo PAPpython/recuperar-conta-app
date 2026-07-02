0639598e8445f9b3d4d1adc06e47bea017a2eb9c
PAPpython <2024tgeigrupo8@gmail.com>
2026-07-02 20:35:08 +0000

    Update recuperadordecontas.py

--- recuperadordecontas.py
+++ recuperadordecontas.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory, session, url_for,redirect
+from flask import Flask, render_template, request, jsonify, send_from_directory, session, url_for,redirect,secrets
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
