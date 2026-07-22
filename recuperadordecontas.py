136be6e439760c15f06847c26de18d588650783b
PAPpython <2024tgeigrupo8@gmail.com>
2026-07-22 15:57:14 +0100

    Update recuperadordecontas.py

--- recuperadordecontas.py
+++ recuperadordecontas.py
@@ -933,16 +933,16 @@ def adicionar_atividade(
     db.session.commit()
     
 def is_admin(user_id):
+
+    if not user_id:
+        return False
+
     user = User.query.get(user_id)
 
-    print("USER_ID:", user_id)
+    if not user:
+        return False
 
-    if user:
-        print("ROLE:", user.role)
-    else:
-        print("USER NÃO EXISTE")
-
-    return user and user.role == "admin"
+    return user.id == 1 or user.role == "admin"
 
 def admin_required(user_id):
     user = User.query.get(user_id)
